9fb2e31444b0154eddfd5e43386ab4adc75f5603
llionaki <lucianos.lionakis@ugent.be>
2022-05-04 14:09:50 +0200

    Implemented the chest opening animation and added a gismo to show the allowed radius to open a chest

--- Assets/Scripts/Chest.cs
+++ Assets/Scripts/Chest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,16 +6,40 @@ using UnityEngine;
 public class Chest : MonoBehaviour
 {
     [SerializeField] private GameObject itemOrb;
+
+    [SerializeField] private Transform chestLid;
+
+    private const float ClosedAngle = 0f;
+    private const float OpenAngle = 55f;
     
-    // Start is called before the first frame update
-    void Start()
+    private float _timer = 0f;
+    private float _time = 0.5f;
+
+    private void Start()
     {
-        
+        enabled = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        _timer += Time.deltaTime;
+        Vector3 chestLidRotation = chestLid.rotation.eulerAngles;
+        chestLidRotation.z = Mathf.LerpAngle(ClosedAngle, OpenAngle, _timer / _time);
+        chestLid.rotation = Quaternion.Euler(chestLidRotation);
+        if (_timer > _time)
+        {
+            enabled = false;
+            gameObject.layer = LayerMask.NameToLayer("OpenContainers");
+            GameObject createdItemOrb = Instantiate(itemOrb, transform.position, transform.rotation);
+            createdItemOrb.transform.Rotate(0, 90, 0);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Color color = Color.gray;
+        color.a = 0.4f;
+        Gizmos.color = color;
+        Gizmos.DrawSphere(transform.position, 1.5f);
     }
 }
